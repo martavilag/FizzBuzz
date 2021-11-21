cb56237739216149617ba136cae5406bee31509e
mvila <mvila@voxelgroup.net>
2021-11-21 23:30:47 +0100

    Created an obvious implementation for test ThreeReturnsFizz and passed

--- FizzBuzz/FizzBuzzChecker.cs
+++ FizzBuzz/FizzBuzzChecker.cs
@@ -11,7 +11,12 @@ namespace FizzBuzz
                 return "1";
             }
 
-            return "2";
+            if (number == 2)
+            {
+                return "2";
+            }
+
+            return "Fizz";
         }
     }
 }
