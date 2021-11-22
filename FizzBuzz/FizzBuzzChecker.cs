56746f0cbc068b3a4e21eabe1b43a45be2699118
mvila <mvila@voxelgroup.net>
2021-11-22 15:51:39 +0100

    refactor: isolated case for 3 and 5 and refactored case for 1, 2 and 4

--- FizzBuzz/FizzBuzzChecker.cs
+++ FizzBuzz/FizzBuzzChecker.cs
@@ -6,25 +6,13 @@ namespace FizzBuzz
     {
         public static string FizzBuzz(int number)
         {
-            if (number == 1)
-            {
-                return "1";
-            }
-
-            if (number == 2)
-            {
-                return "2";
-            }
-
             if (number == 3)
                 return "Fizz";
 
-            if (number == 4)
-            {
-                return "4";
-            }
+            if (number == 5)
+                return "Buzz";
 
-            return "Buzz";
+            return number.ToString();
         }
     }
 }
