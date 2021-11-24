c26de3ce16e8ae710d0927110b640b3e554b4277
mvila <mvila@voxelgroup.net>
2021-11-24 09:01:36 +0100

    feat: nine returns fizz

--- FizzBuzz/FizzBuzzChecker.cs
+++ FizzBuzz/FizzBuzzChecker.cs
@@ -9,11 +9,14 @@ namespace FizzBuzz
             if (number == 3)
                 return "Fizz";
 
-            if (number == 5)
-                return "Buzz";
-
             if (number == 6)
                 return "Fizz";
+
+            if (number == 9)
+                return "Fizz";
+
+            if (number == 5)
+                return "Buzz";
 
             return number.ToString();
         }
